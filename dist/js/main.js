319df486592332954eb04d602f966f2a0bee272d
Rostislav <stillgrey@mail.ru>
2016-11-27 14:04:13 +0200

    loaders

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,85 +1,128 @@
 "use strict";
 
+// переменные из кода
+var $total = $('#total-comp'),
+    $listcomp = $('#list-comp ul'),
+    $news = $('#carousel-news'),
+    $newsimg = $('.news-img img'),
+    $newstitle = $('.news-body h3 a'),
+    $newstext = $('.news-body p'),
+    $newsdate = $('.newsdate'),
+    $newsauth = $('.newsauthor'),
+    $indicators = $('.carousel-indicators');
+
+
+
+
+// loaders
+  var loader = '<div class="loader"></div>';
+  $total.html(loader);
+  $listcomp.html(loader);
+  // $news.html(loader);
+
+
+
+
 $(function() {
   // список компаний
-var getlist = $.getJSON( "http://codeit.pro/frontTestTask/company/getList", function(data) {
+  var requestComp = $.getJSON( "http://codeit.pro/frontTestTask/company/getList", function(data) {
 
     var items = [];
-    $.each( data, function(key, val) {
+    $.each(data, function(key, val) {
       items.push( {key: val});
     });
     var compArr = items[0]['key'];
 
-    // общее кол-во компаний
     var name = compArr.map(item => item.name);
-    $('#total-comp').text(name.length);
 
-    // массив имен
-    var name = compArr.map(item => item.name);
+    // общее кол-во компаний
+     $total.text(name.length);
+
+    // список компаний
     $.each(name, function(key,val){
-        // items.push( "<li id='" + key + "'>" + val + "</li>" );
         items.push( `<a href="${val}"><li>${val}</li></a>` );
       });
-    $('#list-comp ul').html(items);
-    console.log(items);
 
-
-
+    $listcomp.html(items);
 
 
 
     // массив локаций
     var location = compArr.map(item => item.location);
-    console.log(location);
+    // console.log(location);
 
     // массив парнеров
     var partners = compArr.map(item => item.partners);
-    console.log(partners);
+    // console.log(partners);
+
+  });
+
+  // requestComp.done(function( msg ) {
+  //   $( "#log" ).html( msg );
+  // });
+
+
+  // список новостей
+  $.getJSON("http://codeit.pro/frontTestTask/news/getList", function(data) {
+    var items = [];
+    $.each(data, function(key, val) {
+      items.push( {key: val});
+    });
+
+    var newsArr = items[0]['key'];
+    console.log(newsArr);
+
+    // var news = newsArr.map(item => [item.author, item.date, item.description, item.img, item.link]);
+    // console.log(news);
+
+    $.each(newsArr, function(i) {
+       $newsimg.attr('src', this.img);
+       $newstitle.attr('href', `http://${this.link}`);
+       $newstext.text(this.description);
+       // $newsdate.attr(this.date);
+       $newsdate.text(moment.unix(this.date).format("DD.MM.YYYY"));
+       $newsauth.text(this.author);
+       $indicators.append(`<li data-target="#carousel-news" data-slide-to="${i}"></li>`);
+    });
+
+
+      // $('#carousel-news').addClass('active');
+
+      // обрезка текста новости
+      var text = $('.news-body p').text();
+      var sliced = text.slice(0,220);
+      if (sliced.length < text.length) {
+      sliced += '...';
+      }
+      $('.news-body p').text(sliced);
+
+
+    // console.log(newsArr[0]['author']);
+    // console.log(newsArr[3]['link']);
+
+    // timestamp to date
+
+    // var newsdate = moment.unix(1455091071).format("DD.MM.YYYY");
+    // $('.newsdate').text(newsdate);
+
+
 
   });
 
 
 
-// обрезка текста новости
-var text = $('.news-body p').text();
-var sliced = text.slice(0,250);
-if (sliced.length < text.length) {
-sliced += '...';
-}
-$('.news-body p').text(sliced);
+  // custom scroll
 
-
-// custom scroll
-
-$('#list-comp').slimscroll({
-  height: '121px',
-  color: '#333',
-  size: '11px',
-  alwaysVisible: true,
-  railVisible: true,
-  railColor: '#ccc',
-  railOpacity: .8,
-  wheelStep: 10
-});
-
-
-
-// список новостей
-// $.getJSON( "http://codeit.pro/frontTestTask/news/getList", function(data) {
-//   var items = [];
-//   $.each( data, function(key, val) {
-//     items.push( {key: val});
-//   });
-
-//   var newsArr = items[0]['key'];
-//   console.log(newsArr);
-
-
-//   // let [author, date, description, img, link] = newsArr;
-
-//  console.log(newsArr[3]['link']);
-// });
-
+  $('#list-comp').slimscroll({
+    height: '121px',
+    color: '#333',
+    size: '11px',
+    alwaysVisible: true,
+    railVisible: true,
+    railColor: '#ccc',
+    railOpacity: .8,
+    wheelStep: 10
+  });
 
 
 });
